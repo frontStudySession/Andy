e962e7a159f722135121abcfd8c0bfa52df1002b
Andy <needersw@gmail.com>
2024-01-19 02:00:25 +0900

    alias craco 설정 완료

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+const { CracoAliasPlugin } = require("react-app-alias");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAliasPlugin,
+      options: {
+        source: "tsconfig",
+        baseUrl: "./src",
+        tsConfigPath: "./tsconfig.paths.json", // 위에서 작성한 tsconfig.paths.json입니다.
+      },
+    },
+  ],
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { Hello } from "@app/Hello";
+
+function App() {
+  return <Hello />;
+}
+
+export default App;
